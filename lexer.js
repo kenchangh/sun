5d7b5914e9981960acead32f8c47355133eb0973
mavenave <guanhao3797@gmail.com>
2016-12-09 18:17:46 +0800

    somewhat working tokenize

--- lexer.js
+++ lexer.js
@@ -1,10 +1,126 @@
+function TypeValueObj(type, value) {
+  this.type = type;
+  this.value = value;
+  return this;
+}
 
-function lexCode() {
-  
+function TwoSideObj(lhsTypeValue, operator, rhsTypeValue) {
+  this.lhs = lhsTypeValue;
+  this.operator = operator;
+  this.rhs = rhsTypeValue;
+  return this;
+}
+
+function lexVariable(str) {
+  var re = /^[a-zA-Z_][a-zA-Z_0-9]*$/;
+  if (!str.match(re)) return false;
+  return new TypeValueObj('variable', str);
+}
+
+function lexInt(str) {
+  var re = /^-?[0-9]+$/;
+  if (!str.match(re)) return false;
+  return new TypeValueObj('int', parseInt(str, 10));
+}
+
+function lexObject(str) {
+  var typeChecks = [
+    lexInt,
+  ];
+
+  for (var i=0; i < typeChecks.length; i++) {
+    var typeValue = typeChecks[i](str);
+    if (typeValue) {
+      return new TypeValueObj('object', typeValue);
+    }
+  }
+
+  return false;
+}
+
+function lexOperator(str) {
+  var operators = ['+', '-', '*', '/', '^'];
+  // var spaceSeparedOperators = ['OR', 'AND']
+  var re = /^(?:\+|-|\*|\/|\^)$/;
+  if (!str.match(re)) return false;
+  return new TypeValueObj('operator', str);
+}
+
+function tokenizeExpr(str) {
+  var lexChecks = [
+    lexObject,
+    lexVariable,
+    lexOperator,
+  ];
+
+  str = str.trim();
+  var i = 0;
+  var accumulator = str[i];
+  var tokens = [];
+
+  while (i < str.length) {
+    // console.log('acc: '+accumulator)
+
+    for (var j=0; j < lexChecks.length; j++) {
+      var cur = lexChecks[j](accumulator);
+
+      if (cur) {
+        var validToken;
+        i++;
+
+        do {
+          if (str[i] === ' ') {
+            i++;
+            continue;
+          }
+
+          if (i < str.length) {
+            accumulator += str[i];
+            validToken = lexChecks[j](accumulator);
+            // console.log('acc: '+accumulator);
+          }
+
+          if (!validToken) {
+            accumulator = str[i];
+            tokens.push(cur);
+            break;
+          }
+
+          cur = validToken;
+        } while (i < str.length);
+
+        break;
+      }
+    }
+  }
+
+  return tokens;
+}
+
+function parseTokensIntoExpr(tokenList) {
+  var precedence = ['-', '+', '*', '/', '^'];
+
+  var operators = tokenList.filter(function(typeValue) {
+    return typeValue.type === 'operator';
+  });
+  var scores = operators.map(function(typeValue) {
+    return precedence.indexOf(typeValue.value);
+  });
+}
+
+var lexer = new Lexer()
+console.log(lexer._tokenizeExpr('10000 +1 - 1*199/1'));
+
+function lexCode(str) {
+  var lines = str.split(/[\r\n]+/g);
 }
 
 function Lexer() {
-  this.lexCode = lexCode;
+  this.lex = lexCode;
+  this._lexVar = lexVariable;
+  this._lexInt = lexInt;
+  this._lexObject = lexObject;
+  this._tokenizeExpr = tokenizeExpr;
 }
 
-module.exports = lexer;
+module.exports = Lexer;
--- test/lexer.js
+++ test/lexer.js
@@ -1,14 +1,76 @@
-var lexer = require('../lexer');
+var tap = require('tap');
+var Lexer = require('../lexer');
+var lexer = new Lexer();
 
-var expectedTokens = [
-  // statements
-  [
-    // tokens
-    { type: 'symbol', value: 'x' },
-    { type: 'operator', value: 'assigment' },
-    { type: 'object', value: 1 },
-    { type: 'operator', value: 'addition' },
-    { type: 'object', value: 1 },
-  ],
-];
-var actualTokens = new Lexer().lex('x = 1 + 1');
+/* test variable */
+tap.same(lexer._lexVar('_hey'), { type: 'variable', value: '_hey' });
+tap.same(lexer._lexVar('hey'), { type: 'variable', value: 'hey' });
+tap.notOk(lexer._lexVar('5hey'));
+tap.notOk(lexer._lexVar('-hey'));
+
+/* test int */
+tap.same(lexer._lexInt('1'), { type: 'int', value: 1 });
+tap.same(lexer._lexInt('22'), { type: 'int', value: 22 });
+tap.same(lexer._lexInt('-1'), { type: 'int', value: -1 });
+tap.notOk(lexer._lexInt('5hey'));
+tap.notOk(lexer._lexInt('-hey'));
+
+/* test objects */
+tap.same(lexer._lexObject('1'), {
+  type: 'object',
+  value: { type: 'int', value: 1 },
+});
+tap.same(lexer._lexObject('22'), {
+  type: 'object',
+  value: { type: 'int', value: 22 },
+});
+tap.same(lexer._lexObject('-1'), {
+  type: 'object',
+  value: { type: 'int', value: -1 },
+});
+tap.notOk(lexer._lexObject('5hey'));
+tap.notOk(lexer._lexObject('hey'));
+
+/* test arithmetic */
+lexer._lexExpr('1 + 1');
+
+/* test basic assignment here */
+// var actualAst = new Lexer().lex('x = 1 + 1');
+
+// var expectedAst = {
+//   type: 'program',
+//   statements: [
+//     {
+//       type: 'varassign_stmt',
+//       lhs: {
+//         type: 'variable',
+//         value: 'x',
+//       },
+//       rhs: {
+//         type: 'expr',
+//         expressions: [
+//           {
+//             type: 'arith_expr',
+//             lhs: {
+//               type: 'object',
+//               value: {
+                //   type: 'int',
+                //   value: 1,
+                // }
+//             },
+//             rhs: {
+//               type: 'object',
+//               value: {
+                //   type: 'int',
+                //   value: 1,
+                // }
+//             },
+//           }
+//         ]
+//       },
+//     }
+//   ]
+// }
+
+// tap.same(actualAst, expectedAst);
+
