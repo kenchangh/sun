7a1c20e9c66f2f6b50db72f39f0582a266fc1518
mavenave <guanhao3797@gmail.com>
2016-12-12 00:36:37 +0800

    Enter, printing working
    Did it by using the npm package readline-sync. Instead of doing async,
    where the parser will just skip away, do it sync.
    
    Compilation basically happens during parse stage.

--- parser.js
+++ parser.js
@@ -1,4 +1,4 @@
-var readline = require('readline');
+var readlineSync = require('readline-sync');
 var grammar = require('./grammar');
 var Parser = require('jison').Parser;
 
@@ -6,13 +6,13 @@ var Parser = require('jison').Parser;
 var parser = new Parser(grammar);
 var yy = parser.yy;
 
-yy.scope = {};
+yy.context = {};
 
 yy.Assignment = function Assignment(left, right) {
   this.type = 'assignment';
   this.left = left; // variable
   this.right = right; // expression
-  yy.scope[left.name] = right;
+  yy.context[left.name] = right;
 };
 
 yy.Variable = function Variable(name) {
@@ -21,24 +21,21 @@ yy.Variable = function Variable(name) {
   this.indices = [];
 };
 
-yy.KeywordAction = function KeywordAction(keyword, variable) {
+yy.KeywordAction = function KeywordAction(keyword, expression) {
   this.keyword = keyword;
   switch (keyword) {
     case 'Print':
-      console.log(yy.resolveVar(variable));
+      var value = expression.type === 'variable'
+        ? yy.resolveVar(expression)
+        : expression;
+      console.log(value);
       break;
     case 'Enter':
-      var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-      rl.question('prompt: ', function (answer) {
-        console.log(typeof answer);
-        var val = parseFloat(answer, 10);
-        if (val === NaN) val = answer;
-        new yy.Assignment(new yy.Variable(variable), val);
-        rl.close();
-      });
+      var varName = expression.name;
+      var answer = readlineSync.question('');
+      var val = parseFloat(answer);
+      val = isNaN(val) ? answer : val;
+      new yy.Assignment(new yy.Variable(varName), val);
       break;
     default:
       break;
@@ -47,7 +44,7 @@ yy.KeywordAction = function KeywordAction(keyword, variable) {
 
 yy.resolveVar = function resolveVar(expression) {
   if (expression.type === 'variable') {
-    return yy.scope[expression.name];
+    return yy.context[expression.name];
   }
   return expression;
 }
