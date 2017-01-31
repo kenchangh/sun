83d8823fe30a05d3e0ac75410da8bcb681e0e9eb
mavenave <guanhao3797@gmail.com>
2017-01-31 21:58:13 +0800

    working interpreter

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fs = require('fs');
 var path = require('path');
+var yaml = require('js-yaml');
 var Parser = require('jison').Parser;
 
 var grammar = require('./src/grammar');
@@ -24,6 +25,37 @@ gulp.task('run-examples', function() {
       var src = fs.readFileSync(path.join(examplesDir, file));
       compiler.compile(src.toString());
     })
+  });
+});
+
+gulp.task('copy-examples', function() {
+  var examplesDir = './examples';
+
+  fs.readdir(examplesDir, function(err, files) {
+    var sources = files.map(function(file) {
+      console.log(`Copied ${file}`);
+      var src = fs.readFileSync(path.join(examplesDir, file));
+      return {
+        name: file,
+        src: src.toString(),
+      };
+    });
+    var ymlFile = yaml.safeDump(sources);
+    fs.writeFile('./docs/_data/examples.yml', ymlFile);
+
+    files.forEach(function(file) {
+      var examplesDocDir = './docs/examples';
+      var exampleDoc = path.join(examplesDocDir, file);
+      if (!fs.existsSync(exampleDoc)) {
+        fs.mkdirSync(exampleDoc);
+      }
+      var content = `---
+layout: interpreter
+title: Run online
+---
+{% for example in site.data.examples %}{% if example.name == '${file}' %}{{ example.src | strip }}{% endif %}{% endfor %}\n`
+      fs.writeFileSync(path.join(exampleDoc, 'index.html'), content);
+    });
   });
 });
 
